71a60f2233b387eaeaffc7b5142c5202a151d849
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-08 17:43:26 +0100

    Update third_example.c

--- third_example.c
+++ third_example.c
@@ -1,8 +1,3 @@
-/*this code implement an hierarchical structure that is made up by a server and local devices. 
-The server is the process with global rank 0 and it will create the message and it will send in
-broadcast to all the processes.
-All the processes are split into 3 different group. They will print the recived message form the server and they will print it.*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -35,13 +30,17 @@ int main(int argc, char *argv[]) {
         MPI_Comm_rank(comm_group, &group_rank);
         MPI_Comm_size(comm_group, &group_size);
 
-        printf("Processo %d nel gruppo %d. Rank nel gruppo: %d\n", rank, group_number, group_rank);
-        float global_message [4];
+        if (rank == 0) {
+            printf("Processo %d non appartiene a nessun gruppo.\n\n", rank);
+        } else {
+            printf("Processo %d nel gruppo %d. Rank nel gruppo: %d\n", rank, group_number, group_rank);
+        }
+
+        float global_message[4];
         global_message[0] = -1.0;
         global_message[1] = -1.0;
         global_message[2] = -1.0;
         global_message[3] = -1.0;
-         // Inizializza il messaggio del processo con rank 0 globale
 
         if (group_rank == 0) {
             if (rank == 0) {
@@ -56,18 +55,22 @@ int main(int argc, char *argv[]) {
         // Invia il messaggio del processo con rank 0 globale a tutti i gruppi
         MPI_Bcast(&global_message, 4, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-        // Stampa il messaggio ricevuto da ciascun processo nel gruppo
-        printf("Ha ricevuto il messaggio: %f %f %f %f\n\n", global_message[0],global_message[1],global_message[2],global_message[3]);
-
+        if(rank!=0){
+        
+            // Stampa il messaggio ricevuto da ciascun processo nel gruppo
+            printf("Ha ricevuto il messaggio: %f %f %f %f\n\n", global_message[0], global_message[1], global_message[2], global_message[3]);
+        }
         int group_members[group_size];
         MPI_Gather(&rank, 1, MPI_INT, group_members, 1, MPI_INT, 0, comm_group);
 
-        if (group_rank == 0) {
-            printf("Gruppo %d comprende i seguenti membri: ", group_number);
-            for (int i = 0; i < group_size; i++) {
-                printf("%d ", group_members[i]);
+        if(group_number!=-1){
+            if (group_rank == 0) {
+                printf("Gruppo %d comprende i seguenti membri: ", group_number);
+                for (int i = 0; i < group_size; i++) {
+                    printf("%d ", group_members[i]);
+                }
+                printf("\n\n");
             }
-            printf("\n\n");
         }
 
         if (group_number >= 0) {
