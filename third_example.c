f541442c4875bf4e39fdc424116e853b005fc25e
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-08 13:19:51 +0100

    Create third_example.c

---
+++ third_example.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size < 4) {
+        if (rank == 0) {
+            printf("Questo programma richiede almeno 4 processi.\n");
+        }
+    } else {
+        int group_number;
+
+        if (rank == 0) {
+            group_number = -1; // Processo con rank 0 non appartiene a nessun gruppo
+        } else {
+            group_number = (rank - 1) % 3; // Suddividi gli altri processi in 3 gruppi
+        }
+
+        MPI_Comm comm_group;
+        MPI_Comm_split(MPI_COMM_WORLD, group_number, rank, &comm_group);
+
+        int group_rank;
+        int group_size;
+        MPI_Comm_rank(comm_group, &group_rank);
+        MPI_Comm_size(comm_group, &group_size);
+
+        printf("Processo %d nel gruppo %d. Rank nel gruppo: %d\n", rank, group_number, group_rank);
+
+        int global_message = -1; // Inizializza il messaggio del processo con rank 0 globale
+
+        if (group_rank == 0) {
+            if (rank == 0) {
+                // Processo con rank 0 globale inizia il messaggio
+                global_message = 42; // Esempio di messaggio
+            }
+        }
+
+        // Invia il messaggio del processo con rank 0 globale a tutti i gruppi
+        MPI_Bcast(&global_message, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        // Stampa il messaggio ricevuto da ciascun processo nel gruppo
+        printf("Processo %d nel gruppo %d ha ricevuto il messaggio: %d\n", rank, group_number, global_message);
+
+        int group_members[group_size];
+        MPI_Gather(&rank, 1, MPI_INT, group_members, 1, MPI_INT, 0, comm_group);
+
+        if (group_rank == 0) {
+            printf("Gruppo %d comprende i seguenti membri: ", group_number);
+            for (int i = 0; i < group_size; i++) {
+                printf("%d ", group_members[i]);
+            }
+            printf("\n");
+        }
+
+        if (group_number >= 0) {
+            MPI_Comm_free(&comm_group);
+        }
+    }
+
+    MPI_Finalize();
+    return 0;
+}
