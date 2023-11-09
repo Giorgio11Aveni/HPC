ac3d2e05813f04b4ad0dc9ecbe6d619205c5a7e6
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-09 10:18:02 +0100

    Update third_example.c

--- third_example.c
+++ third_example.c
@@ -9,17 +9,19 @@ int main(int argc, char *argv[]) {
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    if (size < 4) {
+    if (size < 7) {
         if (rank == 0) {
-            printf("Questo programma richiede almeno 4 processi.\n");
+            printf("Questo programma richiede almeno 7 processi.\n");
         }
     } else {
         int group_number;
 
         if (rank == 0) {
             group_number = -1; // Processo con rank 0 non appartiene a nessun gruppo
+        } else if (rank >= 1 && rank <= 3) {
+            group_number = 0; // Nuovo gruppo formato dai processi con rank 1, 2, e 3
         } else {
-            group_number = (rank - 1) % 3; // Suddividi gli altri processi in 3 gruppi
+            group_number = (rank - 4) % 3 + 1; // Suddividi gli altri processi in 3 gruppi con numeri 1, 2, e 3
         }
 
         MPI_Comm comm_group;
@@ -32,38 +34,35 @@ int main(int argc, char *argv[]) {
 
         if (rank == 0) {
             printf("Processo %d non appartiene a nessun gruppo.\n\n", rank);
-        } else {
+        } else if(group_number != 0) {
+            printf("Processo %d nel gruppo %d. Rank nel gruppo: %d\n\n", rank, group_number, group_rank);
+        }else{
             printf("Processo %d nel gruppo %d. Rank nel gruppo: %d\n", rank, group_number, group_rank);
         }
 
         float global_message[4];
-        global_message[0] = -1.0;
-        global_message[1] = -1.0;
-        global_message[2] = -1.0;
-        global_message[3] = -1.0;
-
-        if (group_rank == 0) {
-            if (rank == 0) {
-                // Processo con rank 0 globale inizia il messaggio
+       // Processo con rank 0 globale inizia il messaggio
                 global_message[0] = 10.0; // Esempio di messaggio
                 global_message[1] = 11.0;
                 global_message[2] = 12.0;
                 global_message[3] = 13.0;
-            }
+
+       
+
+        // Invia il messaggio solo ai processi nel nuovo gruppo (gruppo 0)
+        if (group_number == 0) {
+            MPI_Bcast(&global_message, 4, MPI_FLOAT, 0, comm_group);
         }
 
-        // Invia il messaggio del processo con rank 0 globale a tutti i gruppi
-        MPI_Bcast(&global_message, 4, MPI_FLOAT, 0, MPI_COMM_WORLD);
-
-        if(rank!=0){
-        
-            // Stampa il messaggio ricevuto da ciascun processo nel gruppo
+        if (rank != 0 ) {
+            // Stampa il messaggio ricevuto da ciascun processo nel gruppo 0
             printf("Ha ricevuto il messaggio: %f %f %f %f\n\n", global_message[0], global_message[1], global_message[2], global_message[3]);
         }
+
         int group_members[group_size];
         MPI_Gather(&rank, 1, MPI_INT, group_members, 1, MPI_INT, 0, comm_group);
 
-        if(group_number!=-1){
+        if (group_number != -1) {
             if (group_rank == 0) {
                 printf("Gruppo %d comprende i seguenti membri: ", group_number);
                 for (int i = 0; i < group_size; i++) {
@@ -73,7 +72,7 @@ int main(int argc, char *argv[]) {
             }
         }
 
-        if (group_number >= 0) {
+        if (group_number >= 0 && group_number != 0) {
             MPI_Comm_free(&comm_group);
         }
     }
