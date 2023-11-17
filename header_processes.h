ceb473aafefae6d688a08a89e9555b5f2928223a
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-17 13:30:49 +0100

    Update header_processes.h

--- header_processes.h
+++ header_processes.h
@@ -10,5 +10,12 @@ int determineGroupNumber(int rank);
 
 void getGroupInfo(MPI_Comm group, int *group_rank, int *group_size);
 
+int getIntermediaryServerNumber(int rank, int group_number);
+
+void broadcastMessage(int rank, int group_number, float *global_message, MPI_Comm intermediary_server);
+
+void printIntermediaryServerProcessDetails(int rank, int intermediary_server_number_associated, int group_number, int group1_rank, int group2_rank, int group3_rank, int group_server_rank, float *global_message);
+
+void printLocalDeviceDetails(int rank, int group_number, int intermediary_server_number_associated, int group1_rank, int group2_rank, int group3_rank, float *global_message);
 
 #endif // FUNCTIONS_H
