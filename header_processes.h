98be45ee5f5b1dd2f2361eb7bb4d97c64ccbf464
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-18 11:56:53 +0100

    Update header_processes.h

--- header_processes.h
+++ header_processes.h
@@ -18,6 +18,14 @@ void printIntermediaryServerProcessDetails(int rank, int intermediary_server_num
 
 void printLocalDeviceDetails(int rank, int group_number, int intermediary_server_number_associated, int group1_rank, int group2_rank, int group3_rank, float *global_message);
 
-void gather_and_print_values(int rank, int group_number, int media, int* gathered_array, int group_rank, int group_size, MPI_Comm intermediary_server);
+void gatherAndPrintValues(int rank, int group_number, int average, int* gathered_array, int group_rank, int group_size, MPI_Comm intermediary_server, int intermediary_server_associated);
+
+int calculateAndPrintAverage(int rank, int gathered_array[], int avg_intermediary_server);
+
+void gatherAndPrintFinalLabel(int rank, int group_number, int* gathered_array0, int* final_gather, int avg_intermediary_server1, int avg_intermediary_server2, int avg_intermediary_server3, int group_server_rank, int group_server_size, MPI_Comm server_group);
+
+void freeIntermediaryCommunicator(int rank, int group_number, MPI_Comm* intermediary_server1, MPI_Comm* intermediary_server2, MPI_Comm* intermediary_server3);
+
+void group_members(int rank, int group_server_size, int group_server_rank, MPI_Comm server_group, int group_number);
 
 #endif // FUNCTIONS_H
