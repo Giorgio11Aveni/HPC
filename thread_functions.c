17750159a5eb0b41cab1fcd66bd1551758fef736
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-20 11:45:59 +0100

    Update thread_functions.c

--- thread_functions.c
+++ thread_functions.c
@@ -3,75 +3,76 @@
 #include "header_thread.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void *calcolaNumero(void *arg) {
-    ThreadData *data = (ThreadData *)arg;
+void* processRows(void* arg) {
+    ThreadData* data = (ThreadData*)arg;
 
-    srand(time(NULL));
+    printf("Thread %d processing %d rows\n", data->threadID, data->rowCount);
 
-    // Calcola il numero
-    int numero_calcolato = 0;
-    if (data->group_number == 1){
-    numero_calcolato = (rand() % 15) + data->group_number;
-    }
-    if (data->group_number == 2){
-    numero_calcolato = (rand() % 15) + data->group_number;
-    }
-    if (data->group_number == 3){
-    numero_calcolato = (rand() % 15) + data->group_number;
+    for (int i = 0; i < data->rowCount; ++i) {
+        // Esegfaui l'elaborazione della riga
+        char* row = data->rows[i];
+        printf("Thread %d - Row %d: %s\n", data->threadID, i + 1, row);
+
+        // Libera la memoria allocata per la riga
+        free(row);
     }
 
-    // Ritorna il risultato come puntatore
-    int *result = malloc(sizeof(int));
-    *result = numero_calcolato;
+    // Libera la memoria allocata per i dati del thread
+    free(data->rows);
+    free(data);
 
-    pthread_exit(result);
+    pthread_exit(NULL);
 }
 
 int group_calculations(int group_number, int rank,int media) {
-     if ((group_number == 1 && rank != 1) || (group_number == 2 && rank != 2) || (group_number == 3 && rank != 3))
-    {
-        // Numero di thread
-        const int num_thread = 5;
-
-        // Array di dati per i thread
-        ThreadData thread_data[num_thread];
-
-        // Array per memorizzare i risultati
-        int *risultati[num_thread];
-
-        // Inizializza i dati e avvia i thread
-        pthread_t threads[num_thread];
-        for (int i = 0; i < num_thread; ++i) {
-            thread_data[i].thread_id = i;
-            thread_data[i].group_number = group_number;
-
-            pthread_create(&threads[i], NULL, (void *(*)(void *))calcolaNumero, (void *)&thread_data[i]);
-
-        }
-
-    // Attendi la fine di tutti i thread e calcola la media dei risultati
-    int somma = 0;
-    for (int i = 0; i < num_thread; ++i) {
-        pthread_join(threads[i], (void **)&risultati[i]);
-        somma += *risultati[i];
+     FILE* file = fopen("C:/Users/tecnico/Desktop/UniME/Materie/HPC/MPI/Programs/Multithreading/X_train.csv", "r");
+    if (!file) {
+        perror("Errore nell'apertura del file");
+        return 1;
     }
 
-    for (int i = 0; i < num_thread; ++i) {
+    int totalRowCount = 135;
+    int threadCount = 5;
+    int rowsPerThread = totalRowCount / threadCount;
+    int remainingRows = totalRowCount % threadCount; // Righe extra da distribuire
 
-        printf("Thread %d: %d\n", i, *risultati[i]); 
-        
+    pthread_t threads[threadCount];
 
-        // Libera la memoria allocata per il risultato
-        free(risultati[i]);
+    for (int i = 0; i < threadCount; ++i) {
+        ThreadData* data = (ThreadData*)malloc(sizeof(ThreadData));
+        data->threadID = i + 1;
+
+        // Calcola il numero di righe che ogni thread deve elaborare
+        data->rowCount = rowsPerThread + (i < remainingRows ? 1 : 0);
+
+        // Alloca memoria per le righe del thread
+        data->rows = (char**)malloc(data->rowCount * sizeof(char*));
+
+        for (int j = 0; j < data->rowCount; ++j) {
+            char line[MAX_LINE_LENGTH];
+            if (fgets(line, sizeof(line), file) == NULL) {
+                perror("Errore nella lettura del file");
+                fclose(file);
+                return 1;
+            }
+
+            // Alloca memoria per la copia della riga
+            data->rows[j] = strdup(line);
+        }
+
+        // Crea il thread
+        if (pthread_create(&threads[i], NULL, processRows, (void*)data) != 0) {
+            perror("Errore nella creazione del thread");
+            return 1;
+        }
     }
 
+    // Attendi la terminazione di tutti i thread
+    for (int i = 0; i < threadCount; ++i) {
+        pthread_join(threads[i], NULL);
+    }
 
-    // Calcola la media
-     media = somma / num_thread;
-
-    printf("Average process %d: %d\n\n", rank, media);
-    return media;
-
-        }
+    fclose(file);
 }
