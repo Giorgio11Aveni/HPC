bef4b8e9d966cb94a8e0fc1353cda77e9bbd715f
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-20 17:01:34 +0100

    Update thread_functions.c

--- thread_functions.c
+++ thread_functions.c
@@ -4,16 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mpi.h>
 
 void* processRows(void* arg) {
-    ThreadData* data = (ThreadData*)arg;
+   ThreadData* data = (ThreadData*)arg;
 
-    printf("Thread %d processing %d rows\n", data->threadID, data->rowCount);
+    printf("MPI Rank %d - Thread %d processing %d rows\n", data->mpiRank, data->threadID, data->rowCount);
 
     for (int i = 0; i < data->rowCount; ++i) {
-        // Esegfaui l'elaborazione della riga
+        // Esegui l'elaborazione della riga
         char* row = data->rows[i];
-        printf("Thread %d - Row %d: %s\n", data->threadID, i + 1, row);
+        printf("MPI Rank %d - Thread %d - Row %d: %s\n", data->mpiRank, data->threadID, i + 1, row);
 
         // Libera la memoria allocata per la riga
         free(row);
@@ -23,56 +24,62 @@ void* processRows(void* arg) {
     free(data->rows);
     free(data);
 
-    pthread_exit(NULL);
+    return NULL;
 }
 
-int group_calculations(int group_number, int rank,int media) {
+int group_calculations(int group_number, int rank,int media, int size) {
+    if (rank > 3){
      FILE* file = fopen("C:/Users/tecnico/Desktop/UniME/Materie/HPC/MPI/Programs/Multithreading/X_train.csv", "r");
     if (!file) {
         perror("Errore nell'apertura del file");
-        return 1;
+        MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
     int totalRowCount = 135;
-    int threadCount = 5;
-    int rowsPerThread = totalRowCount / threadCount;
-    int remainingRows = totalRowCount % threadCount; // Righe extra da distribuire
+    int rowsPerProcess = totalRowCount / size;
+    int remainingRows = totalRowCount % size; // Righe extra da distribuire
+    char line[MAX_LINE_LENGTH];  // Dichiarazione di line
 
-    pthread_t threads[threadCount];
+    // Calcolo dell'offset del cursore del file per iniziare dal processo con rank 4
+    int offset = (rank + size - 4) % size;
 
-    for (int i = 0; i < threadCount; ++i) {
-        ThreadData* data = (ThreadData*)malloc(sizeof(ThreadData));
-        data->threadID = i + 1;
-
-        // Calcola il numero di righe che ogni thread deve elaborare
-        data->rowCount = rowsPerThread + (i < remainingRows ? 1 : 0);
-
-        // Alloca memoria per le righe del thread
-        data->rows = (char**)malloc(data->rowCount * sizeof(char*));
-
-        for (int j = 0; j < data->rowCount; ++j) {
-            char line[MAX_LINE_LENGTH];
-            if (fgets(line, sizeof(line), file) == NULL) {
-                perror("Errore nella lettura del file");
-                fclose(file);
-                return 1;
-            }
-
-            // Alloca memoria per la copia della riga
-            data->rows[j] = strdup(line);
-        }
-
-        // Crea il thread
-        if (pthread_create(&threads[i], NULL, processRows, (void*)data) != 0) {
-            perror("Errore nella creazione del thread");
-            return 1;
+    // Avanza il cursore del file per il blocco di righe destinato a questo processo
+    for (int i = 0; i < offset * rowsPerProcess + (offset < remainingRows ? offset : remainingRows); ++i) {
+        if (fgets(line, sizeof(line), file) == NULL) {
+            perror("Errore nella lettura del file");
+            fclose(file);
+            MPI_Abort(MPI_COMM_WORLD, 1);
         }
     }
 
-    // Attendi la terminazione di tutti i thread
-    for (int i = 0; i < threadCount; ++i) {
-        pthread_join(threads[i], NULL);
+    pthread_t threads;
+
+    ThreadData* data = (ThreadData*)malloc(sizeof(ThreadData));
+    data->mpiRank = rank;
+    data->threadID = rank + 1;
+    data->rowCount = rowsPerProcess + (rank < remainingRows ? 1 : 0);
+    data->rows = (char**)malloc(data->rowCount * sizeof(char*));
+
+    for (int j = 0; j < data->rowCount; ++j) {
+        if (fgets(line, sizeof(line), file) == NULL) {
+            perror("Errore nella lettura del file");
+            fclose(file);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        // Alloca memoria per la copia della riga
+        data->rows[j] = strdup(line);
     }
+
+    // Crea il thread
+    if (pthread_create(&threads, NULL, processRows, (void*)data) != 0) {
+        perror("Errore nella creazione del thread");
+        MPI_Abort(MPI_COMM_WORLD, 1);
+    }
+
+    // Attendi la terminazione del thread
+    pthread_join(threads, NULL);
 
     fclose(file);
+    }
 }
