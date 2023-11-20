7b2dfaa5d0d208559de1a067ad40011760f468fa
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-20 11:45:39 +0100

    Update header_thread.h

--- header_thread.h
+++ header_thread.h
@@ -1,14 +1,18 @@
 // header_thread.h
 
+#include <pthread.h>
+
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-#include <pthread.h>
+#define MAX_LINE_LENGTH 1024
+
 
 // Struttura dati per passare l'ID del thread
 typedef struct {
-    int thread_id;
-    int group_number;
+    int threadID;
+    char** rows;
+    int rowCount;
 } ThreadData;
 
 // Funzione eseguita dai thread
