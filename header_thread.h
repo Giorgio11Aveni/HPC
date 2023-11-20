956431bacba8e0d26e06c96e1eb08b6609f36cff
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-20 17:01:13 +0100

    Update header_thread.h

--- header_thread.h
+++ header_thread.h
@@ -9,15 +9,17 @@
 
 
 // Struttura dati per passare l'ID del thread
+
 typedef struct {
+    int mpiRank;
     int threadID;
     char** rows;
     int rowCount;
 } ThreadData;
 
 // Funzione eseguita dai thread
-void *calcolaNumero(void *arg);
+void *processRows(void *arg);
 
-int group_calculations(int group_number, int rank, int media);
+int group_calculations(int group_number, int rank, int media, int size);
 
 #endif // FUNCTIONS_H
