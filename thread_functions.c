6122fc66a9ccabc6bbf3fa87e478c01e070069c5
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-11-26 17:11:51 +0100

    Update thread_functions.c

--- thread_functions.c
+++ thread_functions.c
@@ -6,34 +6,34 @@
 #include <string.h>
 #include <mpi.h>
 
-#define MAX_ROW_LENGTH 1024
 
-// Funzione eseguita dai thread
+
+// Function performed by threads
 void *threadFunction(void *arg) {
     ThreadData *data = (ThreadData *)arg;
 
-    // Alloca una nuova matrice per il thread
+    // Allocates a new matrix for the thread
     int thread_num_rows = data->end_row - data->start_row;
     float *thread_matrix = (float *)malloc(thread_num_rows * data->num_columns * sizeof(float));
 
-    // Copia le righe corrispondenti dalla matrice locale
+    // Copy the matching rows from the local array
     for (int i = 0; i < thread_num_rows; i++) {
         for (int j = 0; j < data->num_columns; j++) {
             thread_matrix[i * data->num_columns + j] = data->local_matrix[(i + data->start_row) * data->num_columns + j];
         }
     }
 
-    // Stampa la matrice del thread
-    printf("Thread %d stampa righe da %d a %d:\n", data->thread_id, data->start_row, data->end_row);
+    // Print the thread matrix
+    printf("Thread %d prints rows %d to %d:\n", data->thread_id, data->start_row, data->end_row);
 
     for (int i = 0; i < thread_num_rows; i++) {
         for (int j = 0; j < data->num_columns; j++) {
             printf("%f ", thread_matrix[i * data->num_columns + j]);
         }
-        printf("\n");
+        printf("\n\n");
     }
 
-    // Rilascia la memoria allocata per la matrice del thread
+    // Release the memory allocated for the thread matrix
     free(thread_matrix);
 
     pthread_exit(NULL);
@@ -43,35 +43,35 @@ int group_calculations(int rank, int size) {
     
     if (rank>3)
     {
-      // Apertura del file CSV
-    FILE *file = fopen("C:/Users/tecnico/Desktop/UniME/Materie/HPC/MPI/Programs/Federated_Parallel_KNN/X_train.csv", "r");
+      // Open the CSV file
+    FILE *file = fopen(X_TRAIN_PATH, "r");
     if (file == NULL) {
-        fprintf(stderr, "Errore nell'apertura del file CSV.\n");
+        fprintf(stderr, "Error opening the CSV file.\n");
         MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
-    // Determina il numero totale di righe nel file
+    // Determine the total number of rows in the file
     int total_rows = 0;
     char line[MAX_ROW_LENGTH];
     while (fgets(line, MAX_ROW_LENGTH, file) != NULL) {
         total_rows++;
     }
 
-    // Calcola il numero di righe che ogni processo leggerà
+    // Calculate the number of rows each process will read
     int rows_per_process = total_rows / (size-4);
     int remaining_rows = total_rows % (size-4);
 
-    // Calcola la porzione del file da leggere per ogni processo
+    // Calculate the portion of the file to be read for each process
     int start_row = (rank - 4) * rows_per_process + ((rank - 4) < remaining_rows ? (rank - 4) : remaining_rows);
     int end_row = start_row + rows_per_process + ((rank - 4) < remaining_rows ? 1 : 0);
 
-    // Alloca la matrice per memorizzare le righe lette da ciascun processo
-    int num_columns = 4;  // Numero di colonne nel tuo file CSV
+    // Allocate the matrix to store the rows read by each process
+    int num_columns = NFEATURES;  // Number of columns in CSV file
     int num_rows = end_row - start_row;
     float *matrix = (float *)malloc(num_rows * num_columns * sizeof(float));
 
-    // Leggi le righe assegnate al processo corrente
-    fseek(file, 0, SEEK_SET);  // Riporta il cursore al principio del file
+    // Read the rows assigned to the current process
+    fseek(file, 0, SEEK_SET);  // Reset the file cursor to the beginning
     for (int i = 0; i < end_row; i++) {
         if (i >= start_row) {
             fgets(line, MAX_ROW_LENGTH, file);
@@ -81,21 +81,21 @@ int group_calculations(int rank, int size) {
                 token = strtok(NULL, ",");
             }
         } else {
-            fgets(line, MAX_ROW_LENGTH, file);  // Salta le righe non assegnate al processo corrente
+            fgets(line, MAX_ROW_LENGTH, file);  // Skip the rows not assigned to the current process
         }
     }
 
-    // Stampa l'ID del processo
-    printf("Processo %d:\n", rank);
+    // Print the process ID
+    printf("Process %d:\n", rank);
 
-    // Allocazione e inizializzazione dati per i thread
-    pthread_t threads[3];
-    ThreadData thread_data[3];
+    // Allocation and initialization of data for threads
+    pthread_t threads[NTHREADS];
+    ThreadData thread_data[NTHREADS];
 
-    int thread_rows = num_rows / 3;
-    int remaining_thread_rows = num_rows % 3;
+    int thread_rows = num_rows / NTHREADS;
+    int remaining_thread_rows = num_rows % NTHREADS;
 
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < NTHREADS; i++) {
         thread_data[i].thread_id = i;
         thread_data[i].start_row = i * thread_rows + (i < remaining_thread_rows ? i : remaining_thread_rows);
         thread_data[i].end_row = thread_data[i].start_row + thread_rows + (i < remaining_thread_rows ? 1 : 0);
@@ -105,12 +105,12 @@ int group_calculations(int rank, int size) {
         pthread_create(&threads[i], NULL, threadFunction, (void *)&thread_data[i]);
     }
 
-    // Attendi la terminazione di tutti i thread
-    for (int i = 0; i < 3; i++) {
+    // Wait for all threads to finish
+    for (int i = 0; i < NTHREADS; i++) {
         pthread_join(threads[i], NULL);
     }
 
-    // Rilascia la memoria e chiudi il file
+    // Release memory and close the file
     free(matrix);
     fclose(file);  
     }
