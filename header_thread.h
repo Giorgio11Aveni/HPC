6880029ef90baa604b098873a188e3ad123dc591
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-12-03 19:37:01 +0100

    Update header_thread.h

--- header_thread.h
+++ header_thread.h
@@ -10,6 +10,7 @@
 #define NFEATURES 4
 #define MAX_ROW_LENGTH 1024
 #define X_TRAIN_PATH "C:/Users/tecnico/Desktop/MPI/MS_MPI/Programs/Federated_Parallel_KNN/X_train.csv"
+#define Y_TRAIN_PATH "C:/Users/tecnico/Desktop/MPI/MS_MPI/Programs/Federated_Parallel_KNN/y_train.csv"
 
 // Struttura dati per passare argomenti ai thread
 typedef struct {
@@ -17,11 +18,13 @@ typedef struct {
     int start_row;
     int end_row;
     int num_columns;
-    float *local_matrix;
+    float *data_matrix;
+    int *label_matrix;
+    float *test_point;
 } ThreadData;
 // Funzione eseguita dai thread
 void *threadFunction(void *arg);
 
-int group_calculations(int rank, int size);
+int group_calculations(int rank, int size, char *data_filename, char *, float *test_point);
 
 #endif // HEADER_THREAD_H
