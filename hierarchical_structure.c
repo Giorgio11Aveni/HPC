ec6ef5e91a4003b5f6d544004ee40b1221f6ac5a
Giorgio11Aveni <50423009+Giorgio11Aveni@users.noreply.github.com>
2023-12-03 19:37:21 +0100

    Update hierarchical_structure.c

--- hierarchical_structure.c
+++ hierarchical_structure.c
@@ -93,7 +93,7 @@ int main(int argc, char *argv[]) {
         int average = 0;
 
         // Perform group calculations and update average
-        average = group_calculations(rank, size);
+        average = group_calculations(rank, size, X_TRAIN_PATH, Y_TRAIN_PATH,global_message);
         
         
         int gathered_array0[group_server_size]; // The group 0 root process will collect all the arrays into this array of float array
